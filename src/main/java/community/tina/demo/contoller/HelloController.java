63242a09084962b7799e7948876f32027d4d9da4
Tina <slp987692@163.com>
2020-01-20 11:22:13 +0800

    init rep

---
+++ src/main/java/community/tina/demo/contoller/HelloController.java
@@ -0,0 +1,18 @@
+package community.tina.demo.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Tina on 2020/1/20
+ * */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
