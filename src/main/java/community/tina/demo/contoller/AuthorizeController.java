ea4b8978146938acb737017c2a40c24c9a1b40ed
Tina <slp987692@163.com>
2020-01-22 15:18:23 +0800

    add mysql successfully

--- src/main/java/community/tina/demo/contoller/AuthorizeController.java
+++ src/main/java/community/tina/demo/contoller/AuthorizeController.java
@@ -2,16 +2,20 @@ package community.tina.demo.contoller;
 
 import community.tina.demo.dto.AccessTokenDTO;
 import community.tina.demo.dto.GithubUser;
+import community.tina.demo.mapper.UserMapper;
+import community.tina.demo.model.User;
 import community.tina.demo.provider.Githubprovider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * 当点击登录后，github会跳转返回callback请求并返回code和state信息，信息处理
@@ -28,6 +32,10 @@ public class AuthorizeController {
     private String clientid;
     @Value("${github.client.secret}")
     private String clinetsecret;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping("/callback")
     private String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state")String state,
@@ -41,11 +49,18 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientid);
         accessTokenDTO.setClient_secret(clinetsecret);
         String assessToken=githubProvider.getaccesstoken(accessTokenDTO);
-        GithubUser user=githubProvider.getUser(assessToken);
-        if (user!=null)
+        GithubUser githubUser=githubProvider.getUser(assessToken);
+        if (githubUser!=null)
         {
+            User user=new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登陆成功,写入session和cookie
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }
         else
